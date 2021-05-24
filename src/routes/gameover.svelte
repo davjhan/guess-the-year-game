16794086a9086afe04b2b04ae082eb36ebafc9e3
davjhan <davidhaneveloper@gmail.com>
2021-05-24 16:56:27 -0700

    Modified the index page and the gameover page
    + Added grades.

--- src/routes/gameover.svelte
+++ src/routes/gameover.svelte
@@ -5,6 +5,36 @@
     let score = atob($page.query.get('score'))
     if (!score) goto('/')
     export let subPath
+    const ranks = [
+        { grade: 'So bad, it\'s good', atLeast: 0, description: 'It\'s hard to get a score this low on purpose.' },
+        { grade: 'Absolutely Terrible', atLeast: 2, description: 'You should be embarrassed.' },
+        { grade: 'Very Bad', atLeast: 4, description: 'It\'s very bad.' },
+        { grade: 'Bad', atLeast: 5, description: 'It\'s very bad.' },
+        { grade: 'Decent', atLeast: 7, description: 'It\'s an ok score.' },
+        { grade: 'Not Bad', atLeast: 9, description: 'Not bad, but you can do better.' },
+        { grade: 'Average', atLeast: 10, description: 'You should feel satisfied with this score.' },
+        { grade: 'Above Average', atLeast: 11, description: 'This was my personal high score.' },
+        { grade: 'Good', atLeast: 12, description: 'Quite good. You can close the tab now.' },
+        { grade: 'Great', atLeast: 15, description: 'This is very impressive.' },
+        { grade: 'Excellent', atLeast: 20, description: 'Well done! This is the third highest grade.' },
+        { grade: 'Superb', atLeast: 25, description: 'Well done! This is the second highest grade.' },
+        { grade: 'Perfect', atLeast: 30, description: 'This is the highest grade. You probably cheated.' },
+    ]
+
+    function getRank(s) {
+        if (s < ranks[0].atLeast) {
+            return ranks[0]
+        }
+        for (let i = 0; i < ranks.length; i++) {
+            if (i === ranks.length - 1) {
+                return ranks[i]
+            }
+            if (s >= ranks[i].atLeast && s < ranks[i + 1].atLeast) {
+                return ranks[i]
+            }
+        }
+    }
+    const rank = getRank(score)
 </script>
 <style>
 
@@ -15,5 +45,8 @@
         <span class='font-semibold'>Score</span>
         <span class='text-4xl p-2 bg-gray-light px-8 rounded'>{score}</span>
     </div>
+    <span class='text-xl text-center mt-8 '>Grade:
+        <span class='font-semibold bg-secondary py-2 px-4 rounded'>{rank.grade}</span></span>
+    <p class='text-center mt-2'>{rank.description}</p>
     <button class='reg self-stretch mt-8' on:click={()=> goto('/game')}>New Game</button>
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,11 +3,14 @@
 </script>
 <style>
 </style>
-<main class='p-4 flex flex-col items-center'>
-    <h1 class='text-3xl mb-8'>Guess the year game.</h1>
-    <img alt="" class='w-72 ' src="main.png">
+<main class='p-4 flex flex-col '>
+    <h1 class='text-3xl my-16 self-center'>Guess the year game</h1>
+    <span class='font-semibold mb-2 self-center'>Instructions</span>
+    <p class=''>
+        Guess the year that the famous event happened. Answers range between 1900-2021.
+    </p>
 
-    <span class='font-semibold mb-2'>Instructions</span>
-    <p class='text-center'>Guess the year that the famous event happened.</p>
+    <span class='font-semibold mt-8 mb-2 self-center'>Playing Time</span>
+    <p class=''>1-2 minutes.</p>
     <button class='self-center reg mt-8' on:click={()=>goto('/game')}>Start game</button>
 </main>
