63452949c554cc82437f733aabe190426bbbe8a6
davjhan <davidhaneveloper@gmail.com>
2021-05-25 18:49:37 -0700

    Added a 404 fallback to route to davjhan.com

---
+++ src/routes/__error.svelte
@@ -0,0 +1,8 @@
+<script>
+    import 'davjhan-core/src/style.css'
+    import { onMount } from 'svelte'
+
+    onMount(() => {
+        window.location.href = 'https://davjhan.com/404'
+    })
+</script>
