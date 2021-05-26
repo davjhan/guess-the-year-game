740ff5f36538eb7735ce5817851e27eb6eb2a5e3
davjhan <davidhaneveloper@gmail.com>
2021-05-25 19:57:25 -0700

    Added prerender to every page for better SEO.
    Apparently, using adapter-static and fallback makes the app an SPA by default, so I have to explicitly mark every page as prerender.

--- src/routes/game.svelte
+++ src/routes/game.svelte
@@ -1,3 +1,6 @@
+<script context="module">
+    export const prerender = true;
+</script>
 <script lang="ts">
 	import { goto } from '$app/navigation'
 	import { shuffle } from '$lib/utils'
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,6 @@
+<script context="module">
+    export const prerender = true;
+</script>
 <script>
     import { goto } from '$app/navigation'
 
